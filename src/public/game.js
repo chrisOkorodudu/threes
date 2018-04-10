3111b78e57fb735900acd2b30784f9572857fa48
Chris Okorodudu <christopherokorodudu@172-17-10-71.DYNAPOOL.NYU.EDU>
2018-04-09 20:13:46 -0400

    eslint errors fixed

--- src/public/game.js
+++ src/public/game.js
@@ -1,11 +1,10 @@
-computer = [];
-pinsRemaining = 5;
+let computer = [];
+let pinsRemaining = 5;
 
 
-computerTotal = 0;
-playerTotal = 0;
+let computerTotal = 0;
+let playerTotal = 0;
 
-i= 0;
 
 
 document.addEventListener('DOMContentLoaded', init);
@@ -13,14 +12,14 @@ console.log('running');
 
 function init() {
 	console.log('init');
-	var gameDiv = document.getElementById('game'), 
+	const gameDiv = document.getElementById('game'), 
 		overlay = document.querySelector('.overlay');
 
 	gameDiv.style.display = 'none';
 	overlay.style.display = 'none';
 
 
-	var button = document.querySelector('button');
+	const button = document.querySelector('button');
 
 	button.addEventListener('click', Go);
 }
@@ -38,38 +37,38 @@ function Go() {
 
 	initGameUI();
 	const game = document.getElementById('game');
-	var compScore = document.createElement('p');
+	const compScore = document.createElement('p');
 	compScore.id = 'compScore';
 	game.appendChild(compScore);
 
 
 	const startButton = document.getElementById('start');
-	var rollButton = document.getElementById('roll');
+	const rollButton = document.getElementById('roll');
 	const pinButton = document.getElementById('pin');
 	const resetButton = document.getElementById('reset');
 	const dice = document.getElementById('container').childNodes;
-	var results = document.createElement('p');
+	const results = document.createElement('p');
 
 	//creating overlay that will display message conent if 
 	//user tries to make illegal move
-	var overlay = document.createElement('div');
+	const overlay = document.createElement('div');
 	overlay.id = 'overlay';
-	var body = document.querySelector('body');
+	const body = document.querySelector('body');
 	body.appendChild(overlay);
 
 
 //OVERLAY CONTENT
-	var needpin = document.createElement('div');
+	const needpin = document.createElement('div');
 	needpin.textContent = 'You must pin at least one die to continue';
 	overlay.appendChild(needpin);
 	needpin.id = 'needpin';
 	
-	var mustroll = document.createElement('div');
+	const mustroll = document.createElement('div');
 	mustroll.textContent = 'You must roll before you can select a die to pin';
 	overlay.appendChild(mustroll);
 	mustroll.id = 'mustroll';
 
-	var dismissButton = document.createElement('button');
+	const dismissButton = document.createElement('button');
 	dismissButton.textContent = 'Ok. Got it!';
 	dismissButton.id = 'dismiss';
 	overlay.appendChild(dismissButton);
@@ -80,7 +79,7 @@ function Go() {
 	});
 
 
-	let playerScore = document.createElement('p');
+	const playerScore = document.createElement('p');
 	playerScore.id = 'player';
 
 
@@ -128,7 +127,7 @@ function Go() {
 
 //PIN BUTTON CLICK EVENT HANDLER 
 	pinButton.addEventListener('click', () => {
-		onePinned = false;
+		let onePinned = false;
 		dice.forEach((die) => {
 			if (die.classList.contains('pin')) {
 				onePinned = true;
@@ -188,16 +187,14 @@ function Go() {
 		rollButton.disabled = true;
 		pinButton.disabled = true;
 		genComputerScore(computer, diceValues);
-		coputerTotal = 0;
+		computerTotal = 0;
 		playerTotal = 0;
 
-		console.log(i, dice);
 		dice.forEach((die) => {
 			die.classList.toggle('pinned');
 			die.textContent = "";
 		});
-		console.log(dice);
-		i++
+
 		results.style.display = 'none';
 
 		compScore.style.display = 'none';
@@ -208,7 +205,7 @@ function Go() {
 
 
 function playerRoll(diceValues, pinsRemaining) {
-	roll = []
+	let roll = [];
 	if (diceValues.length >= pinsRemaining) {
 		roll = diceValues.splice(0, pinsRemaining);
 	} else {
@@ -221,7 +218,7 @@ function playerRoll(diceValues, pinsRemaining) {
 		});
 	}
 
-	var dice = document.getElementById('container').childNodes;
+	const dice = document.getElementById('container').childNodes;
 
 	for (let i=0; i<5; i++) {
 		if (!dice[i].classList.contains('pinned')) {
@@ -232,12 +229,12 @@ function playerRoll(diceValues, pinsRemaining) {
 
 function displayPlayerScore(dice) {
 	
-	playerScore = document.getElementById('player');
+	const playerScore = document.getElementById('player');
 	playerScore.textContent = 'Player Score: ';
-	score = 0;
+	let score = 0;
 	dice.forEach((die) => {
 		if (die.classList.contains('pinned')) {
-			val = parseInt(die.textContent);
+			const val = parseInt(die.textContent);
 			if (val !== 3) {
 				score += val;
 			}
@@ -252,7 +249,7 @@ function displayPlayerScore(dice) {
 
 
 function displayComputerScore() {
-	compScore = document.getElementById('compScore');
+	const compScore = document.getElementById('compScore');
 	let total = 0;
 	let cScore = "Computer Score: ";
 	for (let i=0; i<computer.length; i++) {
@@ -279,14 +276,14 @@ function displayComputerScore() {
 
 
 function initGameUI() {
-	var game = document.getElementById("game");
+	const game = document.getElementById("game");
 	game.style.display = 'block';
-	var div = document.createElement("div");
+	const div = document.createElement("div");
 
 	div.id = 'container';
 
 	for (let i=0; i<5; i++) {
-		var dice = document.createElement('div');
+		const dice = document.createElement('div');
 		dice.classList.add('dice'); 
 		dice.style.border = 'medium solid #000000';
 		div.appendChild(dice);
@@ -294,28 +291,28 @@ function initGameUI() {
 
 	game.appendChild(div);	
 
-	var buttons = document.createElement('div');
+	const buttons = document.createElement('div');
 	buttons.id = 'buttons';
 
-	var start = document.createElement('button');
+	const start = document.createElement('button');
 	start.textContent = 'Start';
-	start.id = 'start'
+	start.id = 'start';
 	buttons.appendChild(start);
 
-	var roll = document.createElement('button');
+	const roll = document.createElement('button');
 	roll.textContent = 'Roll';
 	roll.disabled = true;
 	roll.id = 'roll';
 	buttons.appendChild(roll);
 
-	var pin = document.createElement('button');
+	const pin = document.createElement('button');
 	pin.textContent = 'Pin';
 	pin.disabled = true;
 	pin.id = 'pin';
 	buttons.appendChild(pin);
 
 	//option to restart game
-	var resetButton = document.createElement('button');
+	const resetButton = document.createElement('button');
 	resetButton.textContent = 'Reset';
 	resetButton.disabled = true;
 	resetButton.id = 'reset';
@@ -329,10 +326,10 @@ function genComputerScore(player, diceValues) {
 	let turnsRemaining = 5;
 	while (turnsRemaining > 0) {
 		if (diceValues.length >= turnsRemaining) {
-			let currentPin = Math.min(...diceValues.splice(0,turnsRemaining));
+			const currentPin = Math.min(...diceValues.splice(0,turnsRemaining));
 			player.push(currentPin);
 		} else {
-			const currentTurn = []
+			const currentTurn = [];
 			for (let i=0; i<(turnsRemaining - (diceValues.length)); i++) {
 				currentTurn.push(Math.floor(Math.random() * Math.floor(6)) + 1);
 			}
@@ -340,11 +337,10 @@ function genComputerScore(player, diceValues) {
 			diceValues.forEach((roll) => {
 				currentTurn.push(roll);
 			});
-			// diceValues = [];s
-			const min = Math.min(...currentTurn);
-			player.push(Math.min(...currentTurn)); 
 
+			player.push(Math.min(...currentTurn)); 
 		}
+		
 		turnsRemaining--;
 	}
 }
